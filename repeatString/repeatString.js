4a9b30f5cd8b4bccd491644296ecc4b50198c75b
Adrian Perez <adrianpevelandia@gmail.com>
2021-01-20 20:11:42 +1030

    First two programs finished

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(entryText,numberTimes) {
+    let resultText="";
+    if(numberTimes<0)
+    resultText="ERROR";
+    for(let i=0;i<numberTimes;i++){
+        resultText+=entryText;
+    }
+   return resultText;
 }
 
 module.exports = repeatString
