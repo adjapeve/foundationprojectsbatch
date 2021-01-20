7091a851ea5e94c869b4bba8fd34e3580c64a498
Adrian Perez <adrianpevelandia@gmail.com>
2021-01-20 20:33:42 +1030

    reversestring program

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (inputString) {
+    let stringToArray = inputString.split("");
+    let result = "";
+    for (let i = stringToArray.length - 1; i >= 0; i--) {
+        result += stringToArray[i];
+    }
+    return result;
 }
 
 module.exports = reverseString
