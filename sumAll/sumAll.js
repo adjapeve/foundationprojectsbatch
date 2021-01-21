135234d1d47f8fcc0837adc2ce9ea7ea9dda09ed
Adrian Perez <adrianpevelandia@gmail.com>
2021-01-21 19:29:13 +1030

    sumALl finished

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(number1,number2) {
+        let sum = 0;
+        let minNum = (number1 < number2) ? number1 : number2;
+        let maxNum = (number2 > number1) ? number2 : number1;
+        if (minNum < 0 || maxNum < 0 || typeof minNum != 'number' || typeof maxNum != 'number')
+            return "ERROR";
+        else {
+            for (let i = minNum; i <= maxNum; i++) {
+                sum += i;
+            }
+            return sum;
+        }
 
-}
+    }
 
 module.exports = sumAll
