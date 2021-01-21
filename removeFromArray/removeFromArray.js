908a067cbd9dfbbd2aeebff2f51b6e92ed5fffb9
Adrian Perez <adrianpevelandia@gmail.com>
2021-01-21 16:35:43 +1030

    removefromarray completed and added prueba file

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (inputArray) {
+    let index = "";
+    let args = Array.from(arguments);
+    let arrayOrigLength = inputArray.length;
+    //iterate targets from arguments
+    for (let i = 1; i <= args.length; i++) {
+        //iterates input string
+        for (let j = 0; j <= arrayOrigLength; j++) {
+            index = inputArray.indexOf(args[i]);
+            if (index >= 0)
+                inputArray.splice(index, 1);
+        }
+    }
+    return inputArray;
 }
 
 module.exports = removeFromArray
