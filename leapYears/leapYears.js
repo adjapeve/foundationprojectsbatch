bd949d3a124105c737ff5741d1f985787e63b417
Adrian Perez <adrianpevelandia@gmail.com>
2021-01-22 19:35:50 +1030

    leap year finished with alternative version

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if(year%4===0){
+        if (year%100===0){
+            if (year%400===0) {
+                return true;
+            }else return false
+        }else return true;
+    }return false;
+    //simplified from internet return ((year%4==0)&&(year%100!=0)||(year%400==0));
 }
 
 module.exports = leapYears
