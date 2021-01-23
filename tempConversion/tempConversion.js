c9a7788e825dcec04792a3874eff050b8fea5873
Adrian Perez <adrianpevelandia@gmail.com>
2021-01-23 20:13:21 +1030

    tempconversion program finished

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(faren) {
+  let result=(faren-32) * (5/9);
+  if (result!=0&&result%2!=0){
+    return parseFloat(result.toFixed(1));
+  }else
+  return result;
 }
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  let result=(celsius*(9/5)) + 32;
+  if (result!=0&&result%2!=0){
+    return parseFloat(result.toFixed(1));
+  }else
+  return result;
 }
 
 module.exports = {
